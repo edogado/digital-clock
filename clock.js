9e050f97f8f08e023c3381201b691da1d12bf0a4
edomorales <edmorales94@gmail.com>
2022-09-11 18:26:51 -0400

    Added a section only for a day instead of having day, month and year in a single line
    changed the logic of the program and deleted unnecessary code

--- clock.js
+++ clock.js
@@ -1,51 +1,48 @@
-const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
-let dateSection = document.getElementById('date');
+let daySection = document.getElementById('day');
 let timeSection = document.getElementById('time');
+let dateSection = document.getElementById('date');
 
 /*---------- get date and set it in the html file -----------------------------------------------------*/
 let setDate = ()=>{
     let today = new Date();
-    //it's faster to load days and months from arrays than using a bunch of ifs
-    dateSection.innerText = `${days[today.getDay()]} ${months[today.getMonth()]} ${today.getDate()}, ${today.getFullYear()}`;
+    //it's faster to load days and months from arrays than using a bunch of if/switch cases
+    daySection.innerText = `${days[today.getDay()]} `
+    dateSection.innerText = `${months[today.getMonth()]} ${today.getDate()}, ${today.getFullYear()}`;
 }
 
 /*---------- get time and set it in the html file -----------------------------------------------------*/
 let time = () => {
     let today = new Date();
     let hrs = today.getHours();//getHours returns values from 0-23
-    let meridiem;
+    let meridiem = 'AM';
 
-    if (hrs === 0){// if we get a 0,
-        hrs = 12;//we set hrs to 12 which is the starting value instead of 0
-        meridiem  = 'AM';// getting a 0 means it's midnight, therefore we get AM
-    }
 
-    else if (hrs < 12){// values less than 12, there's no need to format the hrs
-        meridiem = 'AM';//values from 0-11 gets us the am format
-    }
+    /*---------- Logic for the 12 hrs format --------------------------------------------------*/
+    if (hrs === 0) hrs = 12;//we set hrs to 12 which is the starting value instead of 0; 12 am
 
-    else if(hrs === 12){// if we get a 12, no need to format the hrs; leave it as it is
-        meridiem  = 'PM';//so from 12 and higher, we get pm format
-    }
+    // if we get a 12, no need to format the hrs; leave it as it is
+    if(hrs === 12) meridiem  = 'PM';//so from 12 and higher, we get pm format
 
     //adjusting the hrs to a 12 hrs format with values higher than 12.
-    else {
+    if(hrs > 12) {
         hrs = hrs - 12;//we subtract 12 from the hrs to get in the 1-11 pm range. E.g. 14-12 = 2
         meridiem  = 'PM';//so from 12 and higher, we get pm format
     }
 
+
+    /*---------- logic for the string format of the numbers -----------------------------------*/
     //hrs less than 10 will be printed with a 0 before for aesthetics purposes, e.g. from 4 to 04
-    if (hrs < 10){
-        hrs = '0' + hrs;
-    }
+    if (hrs < 10) hrs = '0' + hrs;
 
     //formatting the minutes for aesthetics purposes
-    let min = today.getMinutes() < 10 ? '0' + today.getMinutes() : today.getMinutes();//numbers less than 10 will be printed with a 0 before, e.g. 04
+    let min = today.getMinutes() < 10 ?  `0${today.getMinutes()}` : today.getMinutes();//numbers less than 10 will be printed with a 0 before, e.g. 04
 
     //formatting the seconds for aesthetics purposes
-    let sec = today.getSeconds() < 10 ? '0' + today.getSeconds() : today.getSeconds();//numbers less than 10 will be printed with a 0 before, e.g. 04
+    let sec = today.getSeconds() < 10 ? `0${today.getSeconds()}` : today.getSeconds();//numbers less than 10 will be printed with a 0 before, e.g. 04
+
 
     //setting values to their respective positions for the html file
     timeSection.innerText = `${hrs}:${min}:${sec} ${meridiem}`;
